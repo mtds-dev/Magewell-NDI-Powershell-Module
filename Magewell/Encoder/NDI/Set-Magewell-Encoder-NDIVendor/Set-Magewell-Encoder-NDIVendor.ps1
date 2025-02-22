c47378cb88ea73d6ce78b31f4e95afa0c01ac068
mtds-dev <dustin@mtds.dev>
2025-02-22 16:52:40 -0500

    Added a new Session parameter to Set-Magewell-Encoder-NDIVendor.

--- Magewell/Encoder/NDI/Set-Magewell-Encoder-NDIVendor/Set-Magewell-Encoder-NDIVendor.ps1
+++ Magewell/Encoder/NDI/Set-Magewell-Encoder-NDIVendor/Set-Magewell-Encoder-NDIVendor.ps1
@@ -22,11 +22,17 @@ function Set-Magewell-Encoder-NDIVendor
     .PARAMETER  Password
      The password to authenticate with.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
      Outputs to JSON Object.
 
     .EXAMPLE
      Set-Magewell-Encoder-NDIVendor -IPAddress "192.168.66.1" -UserName "Admin" -Password "myPassword" -VendorName "vendorName" -VendorID "vendorID"
+
+     Set-Magewell-Encoder-NDIVendor -IPAddress "192.168.66.1" -Session $mySession -VendorName "vendorName" -VendorID "vendorID"
 
     .LINK
      NONE
@@ -44,30 +50,38 @@ function Set-Magewell-Encoder-NDIVendor
         [Parameter(Mandatory = $false)]
         [String]$VendorID,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Warning "Authentication failed, command will not be executed."
             return $null
@@ -101,7 +115,7 @@ function Set-Magewell-Encoder-NDIVendor
         }
 
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to configure NDI Vendor Name"
             SuccessMessage = "Action taken successfully."
