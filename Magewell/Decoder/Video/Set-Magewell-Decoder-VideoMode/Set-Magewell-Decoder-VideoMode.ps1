13842bbbeec103a27428b46421aaecf5f7ff38ba
mtds-dev <dustin@mtds.dev>
2025-02-22 13:51:02 -0500

    Added a new Session parameter to Set-Magewell-Decoder-VideoMode.

--- Magewell/Decoder/Video/Set-Magewell-Decoder-VideoMode/Set-Magewell-Decoder-VideoMode.ps1
+++ Magewell/Decoder/Video/Set-Magewell-Decoder-VideoMode/Set-Magewell-Decoder-VideoMode.ps1
@@ -31,11 +31,17 @@ function Set-Magewell-Decoder-VideoMode
     .PARAMETER  Password
      The password to authenticate with.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
-     NONE
+     Returns a JSON object.
 
     .EXAMPLE
-     NONE
+      Set-Magewell-Decoder-VideoMode -IPAddress "192.168.66.1" -UserName "Admin" -Password "myPassword" -AspectRatio "xxx"
+
+      Set-Magewell-Decoder-VideoMode -IPAddress "192.168.66.1" -Session $mySession -AspectRatio "xxx"
 
     .LINK
      NONE
@@ -60,30 +66,38 @@ function Set-Magewell-Decoder-VideoMode
         [Parameter(Mandatory = $true)]
         [string]$AspectRatio,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("user")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Warning "Authentication failed, command will not be executed."
             return $null
@@ -111,7 +125,7 @@ function Set-Magewell-Decoder-VideoMode
             "&AspectRatio=" + $AspectRatio
     
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to take specified action."
             SuccessMessage = "Action taken successfully."
