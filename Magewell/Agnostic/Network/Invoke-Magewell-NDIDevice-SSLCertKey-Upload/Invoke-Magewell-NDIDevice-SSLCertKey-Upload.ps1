ddade8eea91945ff2dd7ce8e5ec9dba48f27e79a
mtds-dev <dustin@mtds.dev>
2025-02-22 06:39:34 -0500

    Added a new Session parameter to Invoke-Magewell-NDIDevice-SSLCertKey-Upload.

--- Magewell/Agnostic/Network/Invoke-Magewell-NDIDevice-SSLCertKey-Upload/Invoke-Magewell-NDIDevice-SSLCertKey-Upload.ps1
+++ Magewell/Agnostic/Network/Invoke-Magewell-NDIDevice-SSLCertKey-Upload/Invoke-Magewell-NDIDevice-SSLCertKey-Upload.ps1
@@ -16,11 +16,17 @@ function Invoke-Magewell-NDIDevice-SSLCertKey-Upload
     .PARAMETER  Password
       Password of the device
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
       Returns a WebRequestSession.
 
     .EXAMPLE
       Invoke-Magewell-NDIDevice-SSLCertKey-Upload -IPAddress "192.168.66.1" -UserName "Admin" -Password "myPassword"
+
+      Invoke-Magewell-NDIDevice-SSLCertKey-Upload -IPAddress "192.168.66.1" -Session $mySession
 
     .LINK
      NONE
@@ -30,30 +36,38 @@ function Invoke-Magewell-NDIDevice-SSLCertKey-Upload
      #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
 
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Host "Authentication failed, command will not be executed."
             return $null
@@ -63,7 +77,7 @@ function Invoke-Magewell-NDIDevice-SSLCertKey-Upload
         $url = "http://" + $IPAddress + "/mwapi?method=upload-ssl-cert-key"
 
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to upload SSL certificate."
             SuccessMessage = "Action taken successfully, check response."
