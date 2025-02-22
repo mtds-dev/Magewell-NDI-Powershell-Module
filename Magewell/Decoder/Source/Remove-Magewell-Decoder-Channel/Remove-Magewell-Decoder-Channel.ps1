f9fb1acb41e41f28b1d8016015bb090389ec735b
mtds-dev <dustin@mtds.dev>
2025-02-22 12:53:30 -0500

    Add a new Session parameter to Remove-Magewell-Decoder-Channel.

--- Magewell/Decoder/Source/Remove-Magewell-Decoder-Channel/Remove-Magewell-Decoder-Channel.ps1
+++ Magewell/Decoder/Source/Remove-Magewell-Decoder-Channel/Remove-Magewell-Decoder-Channel.ps1
@@ -19,11 +19,17 @@ function Remove-Magewell-Decoder-Channel
     .PARAMETER  Password
      The password to authenticate with.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
-     NONE
+     Returns a JSON object.
 
     .EXAMPLE
      Remove-Magewell-Decoder-Channel -Name MyCameraFeed -IPAddress 10.10.10.10 -UserName Admin -Password myPassword
+
+     Remove-Magewell-Decoder-Channel -Name MyCameraFeed -IPAddress 10.10.10.10 -Session $mySession
 
     .LINK
      NONE
@@ -37,30 +43,38 @@ function Remove-Magewell-Decoder-Channel
         [Alias("SourceName")]
         [string]$Name,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("user")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Warning "Authentication failed, command will not be executed."
             return $null
@@ -84,7 +98,7 @@ function Remove-Magewell-Decoder-Channel
         $url = "http://" + $IPAddress + "/mwapi?method=del-channel&name=" + $Name 
     
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to take specified action."
             SuccessMessage = "Action taken successfully."
