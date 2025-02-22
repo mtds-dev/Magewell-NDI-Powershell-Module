17509283fb1c5418cef7605d3732af061cb22ba7
mtds-dev <dustin@mtds.dev>
2025-02-22 11:22:42 -0500

    Added a new Session parameter to Invoke-Magewell-NDIDevice-ResetVideoConfiguration.

--- Magewell/Agnostic/Video/Invoke-Magewell-NDIDevice-ResetVideoConfiguration/Invoke-Magewell-NDIDevice-ResetVideoConfiguration.ps1
+++ Magewell/Agnostic/Video/Invoke-Magewell-NDIDevice-ResetVideoConfiguration/Invoke-Magewell-NDIDevice-ResetVideoConfiguration.ps1
@@ -16,11 +16,17 @@ function Invoke-Magewell-NDIDevice-ResetVideoConfig
     .PARAMETER  Password
      Password for the device
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
-     NONE
+     Returns a JSON object.
 
     .EXAMPLE
-     Invoke-Magewell-NDIDevice-ResetVideoConfigg -IPAddress 10.10.10.10 -UserName Admin -Password myPassword 
+     Invoke-Magewell-NDIDevice-ResetVideoConfig -IPAddress 10.10.10.10 -UserName Admin -Password myPassword 
+
+    Invoke-Magewell-NDIDevice-ResetVideoConfig -IPAddress 10.10.10.10 -Session $mySession 
 
     .LINK
      NONE
@@ -30,30 +36,38 @@ function Invoke-Magewell-NDIDevice-ResetVideoConfig
      #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @SessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Host "Authentication failed, command will not be executed."
             return $null
@@ -62,7 +76,7 @@ function Invoke-Magewell-NDIDevice-ResetVideoConfig
         $url = "http://" + $IPAddress + "/mwapi?method=reset-video-config"
         
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to reset video configuration."
             SuccessMessage = "Action taken successfully."
