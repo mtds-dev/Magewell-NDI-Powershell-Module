ea99892cf3551f6f4983598685373a6d443cc670
mtds-dev <dustin@mtds.dev>
2025-02-22 06:12:32 -0500

    Added a new Session parameter to Invoke-Magewell-NDIDevice-CloudUnregister.

--- Magewell/Agnostic/Cloud/Invoke-Magewell-NDIDevice-CloudUnRegister/Invoke-Magewell-NDIDevice-CloudUnregister.ps1
+++ Magewell/Agnostic/Cloud/Invoke-Magewell-NDIDevice-CloudUnRegister/Invoke-Magewell-NDIDevice-CloudUnregister.ps1
@@ -22,8 +22,14 @@ function Invoke-Magewell-NDIDevice-CloudUnregister
     .OUTPUTS
       Returns a WebRequestSession.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .EXAMPLE
       Invoke-Magewell-NDIDevice-CloudUnregister -IPAddress "192.168.66.1" -UserName "Admin" -Password "myPassword"
+
+      Invoke-Magewell-NDIDevice-CloudUnregister -IPAddress "192.168.66.1" -Session $mySession
 
     .LINK
      NONE
@@ -36,30 +42,39 @@ function Invoke-Magewell-NDIDevice-CloudUnregister
         [Parameter(Mandatory = $true)]
         [String]$ID, 
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
+
     )
 
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Host "Authentication failed, command will not be executed."
             return $null
@@ -69,7 +84,7 @@ function Invoke-Magewell-NDIDevice-CloudUnregister
         $url = "http://" + $IPAddress + "/cloud-api?method=cloud-unreg-ex&id=1"
 
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Reaching out to the Cloud API... attempting to unregister device form cloud."
             SuccessMessage = "Cloud API connected, check results for status."
