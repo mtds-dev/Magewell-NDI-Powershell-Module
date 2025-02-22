aa978963474eaab82bd52dcfe2948cc5794fa5e8
mtds-dev <dustin@mtds.dev>
2025-02-22 11:30:16 -0500

    Added a new Session parameter to Set-Magewell-Decoder-AudioConfiguration.

--- Magewell/Decoder/Audio/Set-Magewell-Decoder-AudioConfiguration/Set-Magewell-Decoder-AudioConfiguration.ps1
+++ Magewell/Decoder/Audio/Set-Magewell-Decoder-AudioConfiguration/Set-Magewell-Decoder-AudioConfiguration.ps1
@@ -96,11 +96,17 @@ function Set-Magewell-Decoder-AudioConfiguration
     .PARAMETER  Password
      The password to authenticate with.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
-     NONE
+     Returns a JSON object.
 
     .EXAMPLE
-     NONE
+      Set-Magewell-Decoder-AudioConfiguration -IPAddress "192.168.66.1" -UserName "Admin" -Password "myPassword" -Gain 50
+
+      Set-Magewell-Decoder-AudioConfiguration -IPAddress "192.168.66.1" -Session $mySession -Gain 50
 
     .LINK
      NONE
@@ -189,30 +195,38 @@ function Set-Magewell-Decoder-AudioConfiguration
         [Parameter(Mandatory = $false)]
         [bool]$CheckPTS,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
 
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Warning "Authentication failed, command will not be executed."
             return $null
@@ -320,7 +334,7 @@ function Set-Magewell-Decoder-AudioConfiguration
         }
 
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to take specified action."
             SuccessMessage = "Action taken successfully."
