b98c5be217809d6dd694d143fe6defb7312328b5
mtds-dev <dustin@mtds.dev>
2025-02-22 15:43:46 -0500

    Added a new Session parameter to Invoke-Magewell-Encoder-EDIDUpload.

--- Magewell/Encoder/EDID/Invoke-Magewell-Encoder-EDIDUpload/Invoke-Magewell-Encoder-EDIDUpload.ps1
+++ Magewell/Encoder/EDID/Invoke-Magewell-Encoder-EDIDUpload/Invoke-Magewell-Encoder-EDIDUpload.ps1
@@ -16,6 +16,10 @@ function Invoke-Magewell-Encoder-EDIDUpload
     .PARAMETER  Password
      The password to authenticate with.
 
+    .PARAMETER  Session
+     Use a previously created WebRequestSession (Authentication session)
+     Created using Invoke-Magewell-NDIDevice-Authentication. 
+
     .OUTPUTS
      Outputs JSON object.
 
@@ -30,30 +34,37 @@ function Invoke-Magewell-Encoder-EDIDUpload
      #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("IP")]
         [String]$IPAddress = "192.168.66.1",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'New-Session')]
         [Alias("User")]
         [String]$UserName = "Admin",
       
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Pass-Session')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'New-Session')]
         [Alias('Pass')]
-        [String]$Password
+        [String]$Password,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Pass-Session')]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$Session
     )
     
     process
     {
-
-        $sessionArguments = @{
-            IPAddress = $IPAddress
-            UserName = $UserName
-            Password = $Password
+        if ($null -eq $Session)
+        {
+            $SessionArguments = @{
+                IPAddress = $IPAddress
+                UserName = $UserName
+                Password = $Password
+            }
+            $Session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
         }
-        $session = Invoke-Magewell-NDIDevice-Authentication @sessionArguments 
 
-        if ($null -eq $session)
+        if ($null -eq $Session)
         {
             Write-Warning "Authentication failed, command will not be executed."
             return $null
@@ -76,7 +87,7 @@ function Invoke-Magewell-Encoder-EDIDUpload
         $url = "http://" + $IPAddress + "/mwapi?method=upload-edid"
 
         $argumentList = @{
-            Session = $session
+            Session = $Session
             URL = $url
             BeginMessage = "Attempting to upload EDID configuration."
             SuccessMessage = "Action taken successfully."
